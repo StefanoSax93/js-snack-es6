a7efff9ec74228dcada6b761247a55a22954c446
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-06-06 15:05:05 +0200

    aggiunta snack 1

--- snack 1/js/app.js
+++ snack 1/js/app.js
@@ -6,3 +6,53 @@ con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore 
 utilizzando destructuring e template literal
 */
+
+//creo un array di oggetti
+const listaBici = [
+    {
+        nome: 'ciclocross',
+        peso: 8.5
+    },
+    {
+        nome: 'Ebike',
+        peso: 9.5
+    },
+    {
+        nome: 'gravel',
+        peso: 9
+    },
+    {
+        nome: 'scatto fisso',
+        peso: 7.5
+    },
+    {
+        nome: 'cronomertro',
+        peso: 7
+    },
+    {
+        nome: 'endurance',
+        peso: 10
+    }
+];
+
+// devo trovare la bici con peso minore
+const { nome, peso } = listaBici[0];
+
+let biciConPesoMinore = {
+    nome,
+    peso,  
+};
+
+for (let i = 1; i < listaBici.length; i++) {
+
+    const { peso, nome } = listaBici[i];
+
+    if (biciConPesoMinore.peso > peso) {
+    biciConPesoMinore = {
+        nome,
+        peso,
+    };
+}
+};
+
+console.log(`Bici: ${biciConPesoMinore.nome} - Peso: ${biciConPesoMinore.peso}kg`);
